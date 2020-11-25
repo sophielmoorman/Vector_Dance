ba99aa9072a6090e358d9b2c20a67cd1c5903cdf
sophielmoorman <70649942+sophielmoorman@users.noreply.github.com>
2020-11-25 16:21:39 -0500

    Commented functions and passed  in robot to help functions.
    Still need to make asynchronous.

--- vector_dance.py
+++ vector_dance.py
@@ -9,6 +9,7 @@ from anki_vector.util import degrees, distance_mm, speed_mmps
 # import to pass in arguments in command line
 import sys
 
+# connect robot to cube, cube lights up, if robot is connected to cube, robot will try to pick up cube 3 times
 def cube(robot):
     # try to connect to cube 
     robot.world.connect_cube()
@@ -16,12 +17,14 @@ def cube(robot):
     # get cube (if connection worked)
     if robot.world.connected_light_cube:
         # cube should light up
-        robot.behavior.pickup_object(robot.world.connected_light_cube, use_pre_dock_pose=True, num_retries=3)
+        robot.behavior.pickup_object(robot.world.connected_light_cube, num_retries=3)
 
+#make robot sing lyrics to "Stayin Alive"
 def sing(robot):
     robot.behavior.say_text("Ah. Ah. Ah. Ah. Staying alive!")
     robot.behavior.say_text("Ah. Ah. Ah. Ah. Staying alive!")
 
+#robot lifts arm 2x, then spins in a circle, repeat
 def dance(robot):
     # some arm action
     robot.behavior.set_lift_height(0, max_speed=3, duration=3)
@@ -32,10 +35,12 @@ def dance(robot):
     # some arm action
     robot.behavior.set_lift_height(0, max_speed=3, duration=3)
     robot.behavior.set_lift_height(1.0, max_speed=3, duration=3)
+    robot.behavior.set_lift_height(0, max_speed=3, duration=3)
     # turn in place
     robot.behavior.turn_in_place(degrees(180))
     robot.behavior.turn_in_place(degrees(-180))
 
+#robot bobs his head
 def nod(robot):
     robot.behavior.set_head_angle(degrees(0))
     robot.behavior.set_head_angle(degrees(20))
@@ -48,23 +53,23 @@ def main():
     arg = sys.argv[1]    
 
     # open robot like you open a file
-    with anki_vector.Robot(serial=arg,show_viewer=True) as robot:
+    with anki_vector.Robot(serial=arg) as robot:
 
         # sing
         robot.behavior.say_text("I like to sing!")
-        sing()
+        sing(robot)
         
         # nod along to the beat
         robot.behavior.say_text("Sick beat!")
-        nod()
+        nod(robot)
         
         # dance 
         robot.behavior.say_text("I will dance now!")
-        dance()
+        dance(robot)
 
         # light up cube
         robot.behavior.say_text("Where is my cube?")
-        cube()
+        cube(robot)
         
 if __name__ == "__main__":
     main()
